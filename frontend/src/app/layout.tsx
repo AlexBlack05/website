6e3596122dc9821b04bf80f5571f18329d6d50c4
Balkowitsch1 <greysonbalkowitsch@gmail.com>
2024-09-26 18:05:15 -0500

    Title Change back

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -4,7 +4,7 @@ import "@mantine/core/styles.css"
 import { ColorSchemeScript, MantineProvider } from "@mantine/core"
 
 export const metadata = {
-  title: "Bison's Motorsports"
+  title: "Bison Motorsports"
 }
 
 export default function RootLayout({
